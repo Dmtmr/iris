7ab41d7b85b45beabcf72bf1772bdac998219531
dmtmr <diegomart.rmarin@gmail.com>
2025-10-18 23:05:00 +0100

    Remove function temporarily - test basic backend deployment

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,17 +1,9 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { defineFunction } from '@aws-amplify/backend';
-
-// Define the JavaScript Lambda
-const backendLambda = defineFunction({
-  name: 'backend',
-  entry: './amplify/functions/backend',
-});
 
 // Export backend
 export const backend = defineBackend({
   auth,
   data,
-  backendLambda,
 });
