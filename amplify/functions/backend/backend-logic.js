ed0ced1456be359e4e58e49271a376ce97b96a9c
dmtmr <diegomart.rmarin@gmail.com>
2025-10-18 21:50:36 +0100

    backend js

---
+++ amplify/functions/backend/backend-logic.js
@@ -0,0 +1,8 @@
+// Super basic backend logic
+export function processRequest(event) {
+    return {
+      message: "Hello from Node.js backend!",
+      input: event,
+    };
+  }
+  
---
+++ amplify/functions/backend/lambda-function.js
@@ -0,0 +1,13 @@
+import { processRequest } from "./backendLogic.js";
+
+export async function handler(event) {
+  console.log("Received event:", event);
+
+  const result = await processRequest(event);
+
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(result),
+  };
+}
