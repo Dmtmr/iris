5d7a31715b92d36fb62015ebe1525313417bc328
dmtmr <diegomart.rmarin@gmail.com>
2025-10-25 18:13:38 +0100

    changes to runtime resources

--- amplify/functions/lambda-inbound/resource.ts
+++ amplify/functions/lambda-inbound/resource.ts
@@ -1,9 +1,9 @@
-import { defineFunction } from '@aws-amplify/backend';
+import { defineFunction, Runtime } from '@aws-amplify/backend';
 
 export const lambdaInbound = defineFunction({
   name: 'lambda-inbound',
   entry: './src/lambda_function.py',
-  runtime: 'python3.9',
+  runtime: Runtime.PYTHON_3_9, 
   handler: 'lambda_function.lambda_handler',
   environment: {
     DB_HOST: 'email-system-cluster.cluster-csxy24404km6.us-east-1.rds.amazonaws.com',
@@ -15,12 +15,12 @@ export const lambdaInbound = defineFunction({
     S3_BUCKET: 'iris-bucket101425',
     SMTP_PASSWORD: 'AkwoCKgSShGcV0JMRKk+0wlJ3cfeVCXtA1JKIf1GRlH9',
     SMTP_USERNAME: 'AKIAR4MIHURAJUCRYH55',
-    AWS_REGION: 'us-east-1',   
+    AWS_REGION: 'us-east-1',
   },
   vpc: {
     vpcId: 'vpc-0a483b03a3ad5ce23',
     subnetIds: ['subnet-072b8b66feec6f52c', 'subnet-0ffad26057440a18c'],
-    securityGroupIds: ['sg-063d280bc6cea3919']
+    securityGroupIds: ['sg-063d280bc6cea3919'],
   },
   timeout: 120,
   memory: 1024,
